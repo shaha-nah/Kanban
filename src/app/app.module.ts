59464d0e51a3c0dd625189eeacca4c05b493f747
super-hxman <shahanahbp@gmail.com>
2021-04-17 20:44:54 +0400

    drag and drop completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './pages/main-view/main-view.component';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { MainViewComponent } from './pages/main-view/main-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
+
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -9,7 +13,38 @@ export class MainViewComponent implements OnInit {
 
   constructor() { }
 
+  board: Board = new Board('Test board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "Build an awesome application"
+    ]),
+    new Column('Research', [
+      "Lorem ipsum",
+      "foo"
+    ]),
+    new Column('Todo', [
+      "Get to work",
+      "go to sleep"
+    ]),
+    new Column('Done', [
+      "get up",
+      "brush teeth"
+    ])
+  ]);
+
   ngOnInit(): void {
   }
 
+
+  drop(event: CdkDragDrop<string[]>){
+    if (event.previousContainer === event.container){
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }
+    else{
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
 }
